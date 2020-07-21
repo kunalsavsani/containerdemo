1341724559e879c045cef2a0fec45809559df116
kunalsavsani <kunalsavsani@outlook.com>
2020-07-21 16:28:29 +0530

    yaml added

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -13,3 +13,6 @@ namespace crdockerdemo1
         public string Summary { get; set; }
     }
 }
+
+
+//kubectl create secret docker-registry helloworldsn --docker-server=helloworldcts.azurecr.io --docker-username=HelloWorldCTS --docker-password=Ntq/WlhUUW31e4Zj39ZgZHhEEI1bAylu --docker-email=shrivallinew @hotmail.com
